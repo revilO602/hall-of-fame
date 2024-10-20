31d8065ee7d638682a833bf8073988349a8ae0e7
Oliver Leontiev <leontiev.oliver@gmail.com>
2024-10-20 19:12:52 +0200

    index: titles

--- js/index.js
+++ js/index.js
@@ -1,30 +1,31 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const clickableSpan = document.querySelector('.clickable-title');
+    const clickableSpans = document.querySelectorAll('.clickable-title'); // Select all elements with class 'clickable-title'
 
-    function highlightSpan() {
-        clickableSpan.classList.add('highlight');
-    }
+    clickableSpans.forEach(function(clickableSpan) {
+        function highlightSpan() {
+            clickableSpan.classList.add('highlight');
+        }
 
-    clickableSpan.addEventListener('mouseover', function(event) {
-        highlightSpan();
+        clickableSpan.addEventListener('mouseover', function() {
+            highlightSpan();
+        });
+
+        clickableSpan.addEventListener('mouseout', function() {
+            clickableSpan.classList.remove('highlight');
+        });
+
+        clickableSpan.addEventListener('touchstart', function(event) {
+            event.preventDefault(); // Prevent default touch behavior
+            highlightSpan();
+        });
+
+        clickableSpan.addEventListener('touchend', function() {
+            window.location.href = clickableSpan.href; // Navigate to the link
+            clickableSpan.classList.remove('highlight');
+        });
+
+        clickableSpan.addEventListener('click', function(event) {
+            window.location.href = clickableSpan.href; // Navigate to the link
+        });
     });
-
-    clickableSpan.addEventListener('mouseout', function(event) {
-        clickableSpan.classList.remove('highlight');
-    });
-
-    clickableSpan.addEventListener('touchstart', function(event) {
-        event.preventDefault(); // Prevent default touch behavior
-        highlightSpan();
-    });
-
-    clickableSpan.addEventListener('touchend', function(event) {
-        window.location.href = clickableSpan.href; // Navigate to the link
-        clickableSpan.classList.remove('highlight');
-    });
-
-    clickableSpan.addEventListener('click', function(event) {
-        window.location.href = clickableSpan.href; // Navigate to the link
-    });
-
 });
